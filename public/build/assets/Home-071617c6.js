3f595f79cc77afd91ec81209ba0d86ff44492b1b
Zun <malikal.j4@gmail.com>
2023-06-01 13:41:03 +0700

    Build

--- public/build/assets/Home-071617c6.js
+++
@@ -1,1 +0,0 @@
-import{a as r,F as e}from"./app-d185bf6e.js";import o from"./Header-42127dbc.js";import"./menu-bf126a8f.js";function i(){return r(e,{children:r(o,{})})}export{i as default};
---
+++ public/build/assets/Home-8b07c10f.js
@@ -0,0 +1,1 @@
+import{a as r,F as e}from"./app-ae60e0a7.js";import o from"./Header-8c298cb3.js";import"./menu-bf126a8f.js";function i(){return r(e,{children:r(o,{})})}export{i as default};
